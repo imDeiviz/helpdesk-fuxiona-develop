4ea4c8a58e8f30545a495cdec3d0083bc4ea3ca1
imDeiviz <dmrojassantana@gmail.com>
2025-03-24 17:59:04 +0100

    fix import paths in main.jsx and update base path in vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/web/", // Cambia esto si hay problemas con rutas
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
