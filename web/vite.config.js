0bac419535b406d84d28c7744ef10334edb7b281
imDeiviz <dmrojassantana@gmail.com>
2025-03-24 18:10:12 +0100

    update base path in vite.config.js for improved routing

--- web/vite.config.js
+++ web/vite.config.js
@@ -6,7 +6,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/web/", // Cambia esto si hay problemas con rutas
+  base: "./", // Cambia esto si hay problemas con rutas
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
