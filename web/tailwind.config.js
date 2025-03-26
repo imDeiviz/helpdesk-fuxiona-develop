6cc3f156048776aa09dd86fc47432b0310797226
imDeiviz <dmrojassantana@gmail.com>
2025-03-26 16:34:18 +0100

    refactor configuration files for Tailwind CSS and Vite; update export syntax and add comments for clarity

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,6 +1,7 @@
 
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx,tsx,ts,css,scss,html,svg,png,jpg,jpeg}'],
+  // Configuración de Tailwind CSS
+  content: ['./index.html', './src/**/*.{js,jsx,tsx,ts,css,scss,html,svg,png,jpg,jpeg}'], // Archivos que Tailwind debe analizar
   theme: {
     extend: {},
   },
