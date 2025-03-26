b15ffe33c5a11ae2a133b8194cd3f7a7e6189401
imDeiviz <dmrojassantana@gmail.com>
2025-03-26 17:45:43 +0100

    fix import statement for App.css in main.jsx to correct casing

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import './app.css'; // Import custom app styles
-
+import './App.css'; // Import custom app styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AuthProvider } from './context/AuthContext';
 
